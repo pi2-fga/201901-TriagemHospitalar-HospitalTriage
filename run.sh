59569e013c100831d17b675ad011f0803b8cbda9
Mateus de Morais <mateusmorais78@gmail.com>
2019-04-14 11:34:18 -0300

    Add run script

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py runserver 0.0.0.0:8000
+# gunicorn --config /gunicorn.conf --log-config /logging.conf -b :8000 uplugapi.wsgi
