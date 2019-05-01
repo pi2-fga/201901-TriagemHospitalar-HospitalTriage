ea0b0ec1d960b8186f74fbd2fb1e3dae040649b0
Mateus de Morais <mateusmorais78@gmail.com>
2019-05-01 14:33:13 -0300

    Add scripts and use postgres

--- scripts/run/dev.sh
+++ scripts/run/dev.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+sleep 20
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
 python3 manage.py runserver 0.0.0.0:8000
-# gunicorn --config /gunicorn.conf --log-config /logging.conf -b :8000 uplugapi.wsgi
+# gunicorn --config /gunicorn.conf --log-config /logging.conf -b :8000 triage.wsgi
