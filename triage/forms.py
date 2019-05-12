16e41919eb5736dda4faea6a18707d1285bdba79
Fabíola Fleury <fabiola.mfleury@gmail.com>
2019-05-12 16:33:46 -0300

    Restructure css files

--- triage/forms.py
+++ triage/forms.py
@@ -1,24 +1,10 @@
 from django import forms
-from django.forms.widgets import NumberInput
-
-
-class RangeInput(NumberInput):
-    input_type = 'range'
 
 
 class TextForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+    subject = forms.CharField(max_length=100, required=True,
+                              widget=forms.Textarea)
 
 
-class SelectForm(forms.Form):
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea)
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
-
-
-class ScaleForm(forms.Form):
-    number = forms.IntegerField(widget=RangeInput, min_value=0, max_value=10)
+class BooleanForm(forms.Form):
+    boolean = forms.BooleanField(required=True)
--- triage/routes.py
+++ triage/routes.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from boogie.router import Router
+from triage.forms import TextForm, BooleanForm
 
 urlpatterns = Router()
 
@@ -21,4 +22,24 @@ def scale_page(request):
     elif request.method == "POST":
         pass
         # TODO: IMPLEMENT validation
-    return render(request, 'triage/scale_page.html')
+    return render(request, 'triage/pain_scale.html')
+
+
+@urlpatterns.route('text/')
+def text_question(request):
+    form = TextForm()
+    form.fields['subject'].label = "Pergunta top"
+    return render(request, 'triage/text_question.html', {'form': form})
+
+
+@urlpatterns.route('boolean/')
+def boolean_question(request):
+    form = BooleanForm()
+    form.fields['boolean'].label = "Pergunta top"
+    return render(request, 'triage/boolean_question.html', {'form': form})
+
+
+@urlpatterns.route('risk/')
+def pacient_risk(request):
+    return render(request, 'triage/risk_level.html',
+                  {'risk_color': 'red'})
