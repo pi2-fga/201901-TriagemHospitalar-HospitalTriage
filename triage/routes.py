4ffb06655cc20a4daaa54b83d0cd0b09fb1f9905
Fabíola Fleury <fabiola.mfleury@gmail.com>
2019-05-12 14:39:55 -0300

    Create more templates and format them according to prototype

--- triage/routes.py
+++ triage/routes.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from boogie.router import Router
-from triage.forms import ScaleForm
 
 urlpatterns = Router()
 
 
 @urlpatterns.route()
 def welcome_page(request):
-    form = ScaleForm()
-    return render(request, 'triage/welcome_page.html', {'form': form})
+    info = {'title': '',
+            'text': 'Posicione seus pés na marca e clique em iniciar.',
+            'url': '/triage/scale'}
+    return render(request, 'triage/animation.html', info)
 
 
 @urlpatterns.route('scale/')
 def scale_page(request):
-    form = ScaleForm()
-    return render(request, 'triage/form_page.html', {'form': form})
+    if request.method == "POST" and 'options' in request.POST:
+        # value = request.POST['options']
+        # TODO: IMPLEMENT SENDING INFORMATION TO BOT
+        pass
+    elif request.method == "POST":
+        pass
+        # TODO: IMPLEMENT validation
+    return render(request, 'triage/scale_page.html')
