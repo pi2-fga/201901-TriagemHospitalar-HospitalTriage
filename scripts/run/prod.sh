1c5600cfbb4c2376b1639e5ebfd9b00daa8e75ba
Mateus de Morais <mateusmorais78@gmail.com>
2019-07-06 20:34:07 -0300

    Increases timeout in gunicorn run

--- scripts/run/prod.sh
+++ scripts/run/prod.sh
@@ -4,4 +4,4 @@ sleep 20
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --no-input
-gunicorn -b :8000 hospital-triage.wsgi
+gunicorn -b :8000 -t 300 hospital-triage.wsgi
